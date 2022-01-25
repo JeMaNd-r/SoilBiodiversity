d711d33483c6800f7120bed4755763e2acf08d24
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2022-01-25 15:47:20 +0100

    Create occurrence data based on (test) grid

--- src/Occurrences_to_grid.R
+++ src/Occurrences_to_grid.R
@@ -5,24 +5,68 @@
 #                                           #
 #- - - - - - - - - - - - - - - - - - - - - -#
 
-occ <- read.csv(file=paste0(here::here(), "/results/Occurrences_", Taxon_name, ".csv"))
-occ$occ <- 1
-
-# make occurrences as SpatialPoints object
-occ <- occ[occ$SpeciesID==spID, c("x", "y", "occ", "SpeciesID")]
-coordinates(occ) <- ~x+y
-
-
 ## create grid (or load grid)
 r <- raster::raster(xmn=extent_Europe[1], xmx=extent_Europe[2], 
                     ymn= extent_Europe[3], ymx=extent_Europe[4],
-                    res=0.5)
+                    res=0.01, 
+                    crs=CRS("+proj=longlat +datum=WGS84 +ellps=WGS84"))
 
-# define reference system: LAEA
-crs(r) <- CRS("+init=epsg:4238")
+## Load occurrence data
+occ <- read.csv(file=paste0(here::here(), "/results/Occurrences_", Taxon_name, ".csv"))
+occ$occ <- 1
+occ.stack <- raster()
 
-# make points to raster
-occ_grid <- raster::rasterize(occ, r, "occ", fun=sum)
-plot(occ_grid)
+#- - - - - - - - - - - - - - - - - - - - - - - 
+## For loop through all Federal State folders ####
+for(sp in speciesNames$SpeciesID){
+  
+  # ignore errors
+  try({
+          
+  # make occurrences as SpatialPoints object
+  temp.occ <- occ[occ$SpeciesID==sp, c("x", "y", "occ", "SpeciesID")]
+  coordinates(temp.occ) <- ~x+y
+  
+  # make points to raster
+  occ.grid <- raster::rasterize(temp.occ, r, "occ", fun=sum)
+  names(occ.grid) <- sp
 
-plot(occ)
+  # add to raster stack
+  occ.stack <- raster::stack(occ.stack, occ.grid)
+  
+  #plot(occ.grid)
+  
+  print(paste0(sp), " now added to raster stack.")
+  
+  }) # end of try loop
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - 
+## Transform raster stack to occurrence table ####
+occ.points <- rasterToPoints(occ.stack)
+occ.points <- as.data.frame(occ.points)
+
+# # transform in a long data frame
+# occ.points.long <- occ.points %>% pivot_longer(cols=3:ncol(occ.points), names_to = "SpeciesID", values_to = "occ") %>% filter(!is.na(occ))
+# 
+# plot(occ.stack)
+# 
+# ggplot(occ.points.long, aes(x=x, y=y, col=SpeciesID))+
+#   geom_point()
+  
+
+#- - - - - - - - - - - - - - - - - - - - - - - 
+## Save ####
+# # save raster stack
+# raster::writeRaster(occ.stack,file=paste0(here::here(), "/results/OccurrenceGrid_", Taxon_name, ".grd"), format="raster")
+
+# save point data frame
+write.csv(occ.points, file=paste0(here::here(), "/results/Occurrence_rasterized_", Taxon_name, ".csv"))
+
+# # save individual species as own files
+# raster::writeRaster(occ.stack, filename=names(occ.stack), bylayer=TRUE, format="GTiff")
+
+
+
+
+
