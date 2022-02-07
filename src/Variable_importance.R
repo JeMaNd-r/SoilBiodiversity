e60610c9bcd122b58da45aff23dd0da25873b805
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2022-02-07 16:51:57 +0100

    Restructure exisitng and calculate new variable importance

--- src/Variable_importance.R
+++ src/Variable_importance.R
@@ -14,9 +14,25 @@ load(file=paste0(here::here(), "/results/SDM_Models_", spID, ".RData")) #SDMs
 var_imp <- data.frame("Predictor"= predictorNames)
 
 # for loop through all models
-for(i in 1:length(SDMs)){ 
+for(i in 1:length(SDMs)){ try({
   temp.model <- names(SDMs)[[i]]
   number.models <- 1
+  
+  #- - - - - - - - - - - - - - - - - - - - - -
+  ## GLM and GAM ####
+  if(temp.model == "gm_pred" | temp.model =="lm1_pred" | temp.model =="lm_subset_pred"){
+    # calculate VI
+    temp.vi <- caret::varImp(SDMs[[temp.model]][[1]], scale=T) #scaled between 0 and 100% 
+  
+    # check if it looks right
+    #print(temp.vi)
+    
+    # replace Inf values with 0
+    temp.vi[is.infinite(temp.vi[,1]),1] <- 0
+      
+    temp.vi$Predictor <- rownames(temp.vi)
+    colnames(temp.vi)[1] <- temp.model
+  }
   
   #- - - - - - - - - - - - - - - - - - - - - -
   ## Lasso and rigde regression ####
@@ -25,64 +41,103 @@ for(i in 1:length(SDMs)){
     coefs <- coef(SDMs[[temp.model]][[1]]) #!!! simple way...
     
     # remove first element (Intercept)
-    temp.vi <- data.frame("Predictor" = coefs@Dimnames[[1]][coefs@i + 1], temp.model = coefs@x)[-1,]
+    temp.vi <- data.frame("Predictor" = coefs@Dimnames[[1]][coefs@i + 1], temp.model = coefs@x)[-1,] 
     colnames(temp.vi)[2] <- temp.model
     
     temp.vi$Predictor <- stringr::str_split_fixed(temp.vi$Predictor, "_", 2)[,1]
     
-    temp.vi[,2] <- scales::rescale(abs(temp.vi[,2]), c(0,10))
+    temp.vi <- temp.vi %>% group_by(Predictor) %>% summarize_all(mean, na.rm=T) %>% as.data.frame()
     
     # check if it looks right
-    print(temp.vi)
+    #print(temp.vi)
+  }
   
   #- - - - - - - - - - - - - - - - - - - - - -
-  }else{
-    
-    #- - - - - - - - - - - - - - - - - - - - - -
-    ## MARS ####
-    if(temp.model == "mars_pred"){# if necessary, unlist models
-    
-     temp.vi <- SDMs[[temp.model]][[5]]
-     print(temp.vi)
-      
-    }else if(temp.model == ""){
-          
-       # calculate VI
-        temp.vi <- caret::varImp(SDMs[[temp.model]][[1]][,j], scale=T,) #scaled between 0 and 100% 
-        
-        # check if it looks right
-        print(temp.vi)
-        
-        temp.vi$Predictor <- rownames(temp.vi)
-        colnames(temp.vi)[1] <- temp.model
-        
-        # replace NA with 0
-        temp.vi[,2] <- temp.vi[,2] %>% tidyr::replace_na(0)
-        
-        temp.vi.df <- dplyr::full_join(temp.vi.df, temp.vi)
-        
-        rm(temp.vi) 
-      }
-      
-    }else{
-    
-    # calculate VI
-    temp.vi <- caret::varImp(SDMs[[temp.model]][[1]], scale=T) #scaled between 0 and 100% 
+  ## MARS ####
+  if(temp.model == "mars_pred"){# if necessary, unlist models
+  
+   temp.vi <- SDMs[[temp.model]][[5]]
+   temp.vi <- as.data.frame(temp.vi) %>% dplyr::rename("mars_pred" = "Overall")
+   #print(temp.vi)
+  }
+  
+  #- - - - - - - - - - - - - - - - - - - - - -
+  ##  ####    
+  if(temp.model == ""){   
+   # calculate VI
+    temp.vi <- caret::varImp(SDMs[[temp.model]][[1]][,j], scale=T,) #scaled between 0 and 100% 
     
     # check if it looks right
     print(temp.vi)
     
     temp.vi$Predictor <- rownames(temp.vi)
     colnames(temp.vi)[1] <- temp.model
+    
+    # replace NA with 0
+    temp.vi[,2] <- temp.vi[,2] %>% tidyr::replace_na(0)
+    
+    temp.vi.df <- dplyr::full_join(temp.vi.df, temp.vi)
+    
+    rm(temp.vi) 
   }
   
-  # replace NA with 0
-  temp.vi[,2] <- temp.vi[,2] %>% tidyr::replace_na(0)
+  #- - - - - - - - - - - - - - - - - - - - - -
+  ## MaxEnt ####
+  if(temp.model == "maxmod_pred"){
+    
+    temp.results <- SDMs[[temp.model]][[1]]@results
+    temp.vi <- as.data.frame(temp.results[str_detect(rownames(temp.results),"permutation.importance"),])
+    
+    # extract predictor names
+    temp.vi$Predictor <- stringr::str_split_fixed(rownames(temp.vi), "[:punct:]", 2)[,1]
+    colnames(temp.vi)[1] <- temp.model
+    
+    # plot variable importance
+    #plot(SDMs[[temp.model]][[1]])
+    
+    # plot response curve
+    #response(SDMs[[temp.model]][[1]])
+    
+    rm(temp.results)
+  }    
+  
+  
+  #- - - - - - - - - - - - - - - - - - - - - -
+  ## MaxNet ####
+  if(temp.model == "maxnet_pred"){
+      
+    temp.results <- SDMs[[temp.model]][[1]]
+    
+    # use package designed to calculate variable importance of maxnet object
+    #fitMaxnet::varImportance(temp.results)
+    
+    # instead: extract beta coefficients
+    temp.vi <- temp.results$betas
+    temp.vi <- as.data.frame(temp.vi[str_detect(names(temp.vi),"hinge")]) 
+    temp.vi$Predictor <- stringr::str_split_fixed(rownames(temp.vi), "[:punct:]", 3)[,2]
+    
+    temp.vi <- temp.vi %>% group_by(Predictor) %>% summarize_all(mean, na.rm=T) %>% as.data.frame()
+    colnames(temp.vi) <- c("Predictor", temp.model)
+    
+    # plot response curves
+    #plot(temp.results)
+    
+    rm(temp.results)
+  }    
+  
+  # harmonizes predictor column structure
+  temp.vi$Predictor <- as.character(temp.vi$Predictor)
+  
+  # scale variable importance values between 0 and 10
+  temp.vi[,temp.model] <- scales::rescale(abs(temp.vi[,temp.model]), c(0,10))
   
   var_imp <- dplyr::full_join(var_imp, temp.vi)
   
+  # replace NA with 0
+  var_imp[,ncol(var_imp)][is.na(var_imp[,ncol(var_imp)])] <- 0
+  
   rm(temp.vi)
-}
+})}
 
 var_imp
 
@@ -92,7 +147,7 @@ var_imp
 # https://github.com/joaofgoncalves/GoncalvesAna_et_al_2021/tree/master/RCODE/PostModelAnalyses
 
 # Calculate variable importance across all PA sets, eval runs and algorithms 
-varImportance <- get_variables_importance(myBiomodModelOut)
+varImportance <- biomod2::get_variables_importance(myBiomodModelOut)
 #varImportanceByVariableAVG <- apply(varImportance,1,mean)
 #varImportanceByVariableSTD <- apply(varImportance,1,sd)
 
