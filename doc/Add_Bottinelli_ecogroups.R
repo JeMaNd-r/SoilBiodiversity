5241a01412396f01429dfca8abc3874f5450608b
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2022-03-15 12:10:35 +0100

    Start fixing issues in species list
    synonyms, scientist's names, missing taxa (GBIF, Edapho, Phillips)

--- doc/Add_Bottinelli_ecogroups.R
+++ doc/Add_Bottinelli_ecogroups.R
@@ -8,13 +8,18 @@
 # reference: Bottinelli et al. 2020, Geoderma
 
 library(tidyverse)
+library(rgbif)
 
 ## Load data
 # read classification of Bottinelli
 Bottinelli <- read.csv(file="Bottinelli_2020_Earthworm_classification.csv")
 
 # read earthworm species list
-ew_list <- read.csv(file="../doc/Species_list_Crassiclitellata.csv")
+#ew_list <- read.csv(file="../doc/Species_list_Crassiclitellata.csv")
+
+# read raw GBIF data
+load(file=paste0("../results/RawOccurrences_", Taxon_name, ".RData")) #dat
+GBIF <- dat$data; rm(dat)
 
 # read earthworm file from Phillips et a. 2019
 Phillips <- read.csv(file="../data/SppOccData_sWorm_v2.csv")
@@ -22,8 +27,18 @@ Phillips <- read.csv(file="../data/SppOccData_sWorm_v2.csv")
 # read Edaphobase data
 Edapho <- read.csv(file="../data/Edaphobase_download_24-Feb-2021_Lumbricidae_Europe.csv")
 
+
 #- - - - - - - - - - - - - - - - - - - - -
-## Add ecological groups assigned in Phillips et al. 2019 ####
+## Empty earthworm list ####
+
+ew_list <- tibble::tibble("Group_name" = "Earthworms", "Kingdom" = NA, "Phylum" = NA, 
+                      "Class" = NA, "Order" = NA, "Family" = "family", "Genus" = "genus",
+                      "Species" = "species", "SpeciesID" = NA, "Fct_group" = NA, "Comment" = NA,
+                      "Species_synonym" = NA)[0,]
+ew_list
+
+#- - - - - - - - - - - - - - - - - - - - -
+## Load species and ecological groups assigned in Phillips et al. 2019 ####
 
 Phillips <- Phillips %>% 
   dplyr::select(SpeciesBinomial, Ecological_group, Family, Genus) %>% 
@@ -34,7 +49,21 @@ Phillips <- Phillips %>%
 
 Phillips[Phillips$Ecogroup_Phillips=="Unknown", "Ecogroup_Phillips"] <- NA
 
-ew_list2 <- dplyr::full_join(ew_list, Phillips)
+ew_list <- ew_list %>% full_join(Phillips)
+ew_list
+
+#- - - - - - - - - - - - - - - - - - - - -
+## Add species from GBIF ####
+
+GBIF <- GBIF %>%
+  full_join(GBIF %>% dplyr::select(species, #acceptedScientificName, taxonomicStatus,
+                                   kingdom, phylum, order, family, genus) %>% unique(),
+            by=c("Species" = "species", "Kingdom" = "kingdom", "Phylum" = "phylum",
+                 "Order" = "order", "Family" = "family", "Genus" = "genus"))
+
+ew_list <- ew_list %>% full_join(GBIF %>% dplyr::select(species) %>% unique(), 
+                                 by = c("Species" = "species"))
+ew_list
 
 #- - - - - - - - - - - - - - - - - - - - -
 ## Add species from Edaphobase ####
@@ -49,12 +78,62 @@ Edapho <- Edapho %>% dplyr::select(Species) %>% unique() %>%
 
 Edapho$Genus <- word(Edapho$Species, 1, 1)
 
-ew_list2 <- dplyr::full_join(ew_list2, Edapho %>% filter(!Species %in% ew_list2$Species))
+ew_list <- dplyr::full_join(ew_list, Edapho %>% filter(!Species %in% ew_list$Species), by=c("Species")) %>% unique()
+ew_list
 
 # - - - - - - - - - - - - - - - - - -
 ## Add missing species ID ####
-ew_list2$SpeciesID <- paste0(substr(word(ew_list2$Species, 1), 1, 4), "_", 
-                               substr(word(ew_list2$Species, 2), 1, 4))
+ew_list$SpeciesID <- paste0(substr(word(ew_list$Species, 1), 1, 4), "_", 
+                               substr(word(ew_list$Species, 2), 1, 4))
+
+#- - - - - - - - - - - - - - - - - - - - -
+## Define SpeciesID while accounting for synonyms ####
+
+# delete species with Identifier name as species name (e.g., Lumbricus Linneous)
+ew_list <- ew_list %>% filter(!str_detect(Species, " [:upper:]"))
+
+#tax_key <- rgbif::name_suggest(q=Taxon_name, rank=Taxon_rank)
+
+# fix synonyms using ITIS (Integrated Taxonomic Information System) in taxize package
+temp <- taxize::synonyms(ew_list$Species, db="itis")
+synonym_ids <- grep(pattern = "acc_name", temp) #is this the optimal solution?
+#accepted_names <- unlist(cbind(lapply(temp[synonym_ids], '[', "acc_name"), lapply(temp[synonym_ids], '[', "syn_name")), use.names = FALSE)
+all_names <- lapply(temp[synonym_ids], cbind)
+#ew_list$Species[synonym_ids] <- accepted_names
+ew_list <- ew_list %>% full_join(all_names %>% dplyr::select(acc_name, syn_name),
+                                 by=c("Species" = "acc_name")) 
+
+# fix misspellings
+species2 <- unique(species)
+temp <- taxize::gnr_resolve(species2, best_match_only = TRUE, canonical = TRUE)
+temp
+
+species2 <- temp$matched_name2
+# here We will need to recover repeated species in an eficient way, as the are dropped.
+
+# keep accepted names only
+taxize::itis_acceptname(get_tsn(species2))
+vapply(x, itis_acceptname, "")
+
+out <- list()
+for(i in 1:length(species2)){
+  out[[i]] <- itis_acceptname(get_tsn(species2[i]))
+}
+#All accepted, wich is not what I want.
+
+#this provides nicer output and can be used to drop unknown species, AND keep synonims.
+taxas <- tax_name(query = species2, get = "species", verbose = TRUE)
+#fails because not all has species. in a for loop will work.
+out <- list()
+for(i in 1:length(species2)){
+  out[[i]] <- tax_name(species2[i], get = "species")
+}
+out2 <- plyr::ldply(out, data.frame)
+species2[-which(is.na(out2$species))]
+
+#note, using genus do not work, because all has genus now.
+taxas <- tax_name(query = species2, get = "genus", verbose = TRUE)
+
 
 # - - - - - - - - - - - - - - - - - -
 ## Add Bottinelli to species list ####
@@ -65,7 +144,7 @@ Bottinelli$SpeciesID <- paste0(substr(Bottinelli$Genus, 1,4), "_",
                                substr(Bottinelli$Species, 1,4))
 
 # check how different the SpeciesID's are
-setdiff(Bottinelli$SpeciesID, ew_list2$SpeciesID)
+setdiff(Bottinelli$SpeciesID, ew_list$SpeciesID)
 
 ## merge Buoche species and genus name
 Bottinelli$Species_Bouche <- NA
@@ -95,7 +174,7 @@ for(i in 1:nrow(Bottinelli)){
 # try with [genus] [species] names
 Bottinelli$Species <- paste0(Bottinelli$Genus, " ", Bottinelli$Species)
 
-setdiff(Bottinelli$Species, ew_list2$Species)
+setdiff(Bottinelli$Species, ew_list$Species)
 setdiff(Bottinelli$Species_Bouche, ew_list$Species)
 
 # keep only unique rows based on some criteria
@@ -111,7 +190,7 @@ for(i in unique(Bottinelli$Species)){
     temp.species <- !stringr::str_detect(data$Subspecies_Bouche, data$Species)
   }  
   # average percentages
-  temp.data <- data %>% select(Species, Percent_Epigeic, Percent_Anecic, Percent_Endogeic) %>% 
+  temp.data <- data %>% dplyr::select(Species, Percent_Epigeic, Percent_Anecic, Percent_Endogeic) %>% 
     group_by(Species) %>% summarise_all(mean)
 
   # take most frequently used Botinelli group for this species
@@ -124,37 +203,37 @@ for(i in unique(Bottinelli$Species)){
 ew_data
 
 ## Add ecological groups to ew_list
-ew_list2 <- dplyr::left_join(ew_list2, ew_data)
+ew_list <- dplyr::left_join(ew_list, ew_data)
 
 #- - - - - - - - - - - - - - - - - - - - -
 ## Create summarizing eco-group column ####
-ew_list2$Ecogroup <- NA
+ew_list$Ecogroup <- NA
 
-for(i in 1:nrow(ew_list2)){
-  temp_row <- ew_list2[i,]
+for(i in 1:nrow(ew_list)){
+  temp_row <- ew_list[i,]
   
   temp_group <- temp_row$Ecogroup_Bottinelli
   
   # if there is no Ecogroup_Bottinelli, take Phillips
   if(is.na(temp_group)) temp_group <- temp_row$Ecogroup_Phillips
   
-  ew_list2[i,"Ecogroup"] <- temp_group
+  ew_list[i,"Ecogroup"] <- temp_group
 }
 
 
 #- - - - - - - - - - - - - - - - - - - - -
 ## Add family if missing ####
 
-Phylogeny <- ew_list2 %>% 
+Phylogeny <- ew_list %>% 
   dplyr::select(Group_name, Kingdom, Phylum, Class, Order, Family, Genus) %>%
   unique() %>%
   filter(!is.na(Family), !is.na(Kingdom))
 
-ew_list2 <- dplyr::inner_join(ew_list2, Phylogeny)
+ew_list <- dplyr::inner_join(ew_list, Phylogeny)
 
 #- - - - - - - - - - - - - - - - - - - - -
 ## Save expanded ew_list ####
-ew_list2 <- ew_list2 %>% arrange("Order", "Family", "Genus", "Species")
+ew_list <- ew_list %>% arrange("Order", "Family", "Genus", "Species")
 
-write.csv(ew_list2, file="../data/Species_list_Crassiclitellata.csv", row.names = F)
+write.csv(ew_list, file="../data/Species_list_Crassiclitellata.csv", row.names = F)
 
