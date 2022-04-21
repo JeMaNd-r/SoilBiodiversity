c62ea4e7a18f5c95fc5c60b079db0a41fa966eb7
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2022-04-21 13:13:36 +0200

    Fix uncertainty (not from species_stack but from species-specific models)

--- src/Uncertainty_analysis.R
+++ src/Uncertainty_analysis.R
@@ -5,17 +5,39 @@
 #                                           #
 #- - - - - - - - - - - - - - - - - - - - - -#
 
-# load species_stack
-load(file=paste0(here::here(), "/results/_Maps/SDM_stack_bestPrediction_binary_", Taxon_name, ".RData")) 
+# load species-specific models
+load(file=paste0(here::here(), "/results/_Maps/SDM_Predictions_", Taxon_name, "_", spID, ".RData")) #model_list
+
+# load environmental space as data frame
+load(paste0(here::here(),"/results/EnvPredictor_2km_df_normalized.RData")) #Env_norm_df
+
+# create empty data frame
+uncertain_df <- Env_norm_df %>% dplyr::select(x, y)
+
+# loop through all models
+for(m in 1:length(model_list)){ try({
+  # load model prediction
+  temp_model <- model_list[[m]]
+  
+  # rename layer column
+  temp_model[,names(model_list)[m]] <- temp_model$layer
+  temp_model <- temp_model[,c("x","y",names(model_list)[m])]
+  
+  # add species dataframe to stacked dataframe
+  uncertain_df <- uncertain_df %>% full_join(temp_model, by=c("x","y"))
+  
+})}
+
+head(uncertain_df)
 
 # calculate sd of predictions
-species_stack$SD <- apply(species_stack %>% dplyr::select(-x, -y, -Richness),1, sd, na.rm = TRUE)
+uncertain_df$SD <- apply(uncertain_df %>% dplyr::select(-x, -y), 1, sd, na.rm = TRUE)
 
-head(species_stack)
+head(uncertain_df)
 
 # view uncertainty in map 
 #png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"), width=1000, height=1000)
-ggplot(data=species_stack, aes(x=x, y=y, fill=SD))+
+ggplot(data=uncertain_df, aes(x=x, y=y, fill=SD))+
   geom_tile()+
   ggtitle("SD between model predictions")+
   scale_fill_viridis_c()+
@@ -23,3 +45,5 @@ ggplot(data=species_stack, aes(x=x, y=y, fill=SD))+
   theme(axis.title = element_blank(), legend.title = element_blank(),
         legend.position = c(0.1,0.4))
 dev.off()
+
+
