f1135de980ec3d18023b8568de75bef69a2b190d
JeMaNd-r <53231180+JeMaNd-r@users.noreply.github.com>
2022-04-21 09:35:58 +0200

    Create Uncertainty analysis

---
+++ src/Uncertainty_analysis.R
@@ -0,0 +1,25 @@
+#- - - - - - - - - - - - - - - - - - - - - -#
+#                                           #
+#         Uncertainty analysis              #
+#          author: Romy Zeiss               #
+#                                           #
+#- - - - - - - - - - - - - - - - - - - - - -#
+
+# load species_stack
+load(file=paste0(here::here(), "/results/_Maps/SDM_stack_bestPrediction_binary_", Taxon_name, ".RData")) 
+
+# calculate sd of predictions
+species_stack$SD <- apply(species_stack %>% dplyr::select(-x, -y, -Richness),1, sd, na.rm = TRUE)
+
+head(species_stack)
+
+# view uncertainty in map 
+#png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"), width=1000, height=1000)
+ggplot(data=species_stack, aes(x=x, y=y, fill=Richness))+
+  geom_tile()+
+  ggtitle("SD between model predictions")+
+  scale_fill_viridis_c()+
+  theme_bw()+
+  theme(axis.title = element_blank(), legend.title = element_blank(),
+        legend.position = c(0.1,0.4))
+dev.off()
