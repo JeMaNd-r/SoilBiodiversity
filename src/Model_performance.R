7962eab53fd91684e098beb1dcb8855e78975efc
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-05-18 09:26:22 +0200

    Fix model performance

--- src/Model_performance.R
+++ src/Model_performance.R
@@ -11,7 +11,7 @@
 ## Model performance ####
 #- - - - - - - - - - - - - - - - - - - - -
 
-mod_eval <- data.frame(species = NA, # name of the species (more important for later)
+mod_eval_template <- data.frame(species = NA, # name of the species (more important for later)
                        roc = NA, # AUC roc
                        prg = NA, # AUC prg
                        cor = NA, # correlation between predicted & true values
@@ -42,6 +42,7 @@ sdm_names <- c("gm", "lm1", "lm_subset", "lasso", "ridge", "mars", "maxent", "ma
 
 for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 5,]$SpeciesID)){ try({
 print(paste0("Species: ", spID))
+mod_eval <- mod_eval_template
 
 for(i in 1:length(sdm_names)){ try({ 
   temp.model <- sdm_names[i]
@@ -50,8 +51,8 @@ for(i in 1:length(sdm_names)){ try({
   print("=====================================")
   print(temp.model)
 
-  try(temp_sdm <- get(load(file=paste0(here::here(), "/results/", Taxon_name, "/temp_files/SDM_",temp.model,"_", spID, ".RData"))), silent=T)
-  if(exists("temp_sdm")==T){  
+  temp_sdm <- get(load(file=paste0(here::here(), "/results/", Taxon_name, "/temp_files/SDM_",temp.model,"_", spID, ".RData")))
+  #if(exists("temp_sdm")==T){  
 
   #- - - - - - - - - - - - - - - - - - - 
   ## calculate statistics for BIOMOD ####
@@ -68,52 +69,11 @@ for(i in 1:length(sdm_names)){ try({
     lapply(temp.files, load, .GlobalEnv)
     
     prediction <- temp_sdm[["validation"]]
-
-    # doesn't work because there is no inout validation data... 
-    # you may have to run biomod2 twice, one time with full env. space and one time with validation dataset...
-    try({
-      precrec_obj <- precrec::auc(precrec::evalmod(scores = prediction$layer, 
-                                                     labels = validation[,c("x","y","SpeciesID", "occ")]$occ))
-      temp.prg <- prg::calc_auprg(prg::create_prg_curve(labels = validation[,c("x","y","SpeciesID", "occ")]$occ, 
-                                                               pos_scores = prediction$layer))
-      
-      prg_curve <- prg::create_prg_curve(labels = validation[,"occ"], pos_scores = prediction$layer)
-        
-      
-      ## TSS and Kappa
-      roc.pred <- prediction(prediction[names(prediction) %in% rownames(validation)], validation[rownames(validation) %in% names(prediction),"occ"])
-      roc.perf <- ROCR::performance(roc.pred, measure = "tpr", x.measure = "fpr")
-      
-      # calculate Sensitivity and Specificity
-      sen_spe <- opt.cut(roc.perf, roc.pred)
-      
-      # calculate TSS
-      temp.tss <- as.numeric(sen_spe [1,1] +  sen_spe[2,1] - 1 )
-      
-      # calculate Kappa
-      mod.object <- sdm::evaluates(x = validation[,"occ"], p = prediction)
-      temp.kappa <- mod.object@threshold_based[mod.object@threshold_based$criteria=="max(se+sp)", "Kappa"] #kappa at max(se+sp)
-      temp.tresh <- mod.object@threshold_based[mod.object@threshold_based$criteria=="max(se+sp)", "threshold"] #threshold max(se+sp)
-    }, silent=T)
     
     # save in output data frame
+    mod_eval[i,]$model <- sdm_names[i]
     mod_eval[i,]$species <- spID
-    try(mod_eval[i,]$roc <- precrec_obj[precrec_obj$curvetypes=="ROC", "aucs"], silent=T)
-    #try(mod_eval[i,]$roc <- myBiomodModelEval["ROC", "Testing.data"])
-    try(mod_eval[i,]$prg <- temp.prg, silent=T)
-    #try(mod_eval[i,]$prg <- NA)
-    try(mod_eval[i,]$cor <- cor(prediction, validation$occ), silent=T)
-    #try(mod_eval[i,]$cor <- NA)
-    #try(mod_eval[i,]$tss <-  myBiomodModelEval["TSS", "Testing.data"])
-    try(mod_eval[i,]$tss <- temp.tss, silent=T)
-    #try(mod_eval[i,]$kappa <-  myBiomodModelEval["KAPPA", "Testing.data"])
-    try(mod_eval[i,]$kappa <- temp.kappa, silent=T)
-    
-    # threshold at max TSS (Note: biomod predicion not scaled, their range is 0-1000
-    #try(mod_eval[i,]$thres.maxTSS <-  myBiomodModelEval[2,2] / 1000, silent=T) 
-    try(mod_eval[i,]$thres.maxTSS <-  temp.tresh, silent=T) # threshold at max(se+sp)
      
-    mod_eval[i,]$model <- temp.model
     try(mod_eval[i,]$time_model <- paste0(temp_sdm[["time_model"]][1], " ", temp_sdm[["time_model"]][2]), silent=T)
     try(mod_eval[i,]$time_predict <- paste0(temp_sdm[["time_predict"]][1], " ", temp_sdm[["time_predict"]][2]), silent=T)
     mod_eval[i,]$bg <- modelName
@@ -121,12 +81,12 @@ for(i in 1:length(sdm_names)){ try({
     try(mod_eval[i,]$n_vali_presence <- sum(validation$occ), silent=T)
     try(mod_eval[i,]$n_vali_absence <- (length(validation$occ) - sum(validation$occ)), silent=T)
     
-    if(is.na(mod_eval[i,]$tss)==T) mod_eval[i,]$tss <- myBiomodModelEval[2,1]
-    if(is.na(mod_eval[i,]$roc)==T) mod_eval[i,]$roc <- myBiomodModelEval[3,1]
-    if(is.na(mod_eval[i,]$kappa)==T) mod_eval[i,]$kappa <- myBiomodModelEval[1,1]
-    if(is.na(mod_eval[i,]$thres.maxTSS)==T) mod_eval[i,]$thres.maxTSS <- myBiomodModelEval[2,2] / 1000    
+    mod_eval[i,]$tss <- myBiomodModelEval[2,1]
+    mod_eval[i,]$roc <- myBiomodModelEval[3,1]
+    mod_eval[i,]$kappa <- myBiomodModelEval[1,1]
+    mod_eval[i,]$thres.maxTSS <- myBiomodModelEval[2,2] / 1000    
 
-    rm(prg, temp.model, modelName, precrec_obj, temp_sdm, validation, temp.tss, temp.kappa, temp.tresh, myBiomodModelEval, prediction)
+    rm(prg, temp.model, modelName, precrec_obj, validation, temp.tss, temp.kappa, temp.tresh, myBiomodModelEval, prediction, modelName)
     
   }else{ # all except BIOMOD modeling
     
@@ -207,14 +167,13 @@ for(i in 1:length(sdm_names)){ try({
     }, silent=T)
     
     # save in output data frame
-    mod_eval[i,]$species <- spID
     try(mod_eval[i,]$roc <- precrec::auc(precrec_obj)[1,4], silent=T)
     try(mod_eval[i,]$prg <- prg::calc_auprg(prg_curve), silent=T)
     try(mod_eval[i,]$cor <- cor(prediction,  validation[,"occ"]), silent=T)
     try(mod_eval[i,]$tss <- temp.tss, silent=T)
     try(mod_eval[i,]$kappa <- temp.kappa, silent=T)
     try(mod_eval[i,]$thres.maxTSS <-  temp.tresh, silent=T) # threshold at max(se+sp)
-    mod_eval[i,]$model <- temp.model
+    mod_eval[i,]$model <- sdm_names[i]
     try(mod_eval[i,]$time_model <- paste0(temp_sdm[["time_model"]][1], " ", temp_sdm[["time_model"]][2]), silent=T)
     try(mod_eval[i,]$time_predict <- paste0(temp_sdm[["time_predict"]][1], " ", temp_sdm[["time_predict"]][2]), silent=T)
     mod_eval[i,]$bg <- modelName
@@ -222,38 +181,26 @@ for(i in 1:length(sdm_names)){ try({
     try(mod_eval[i,]$n_vali_presence <- sum(validation$occ), silent=T)
     try(mod_eval[i,]$n_vali_absence <- (length(validation$occ) - sum(validation$occ)), silent=T)
   
-    rm(temp.kappa, temp.tss, temp.tresh, sen_spe, temp.model, modelName, precrec_obj, temp_sdm, validation, temp.files, prediction)
+    rm(temp.kappa, temp.tss, temp.tresh, sen_spe, temp.model, modelName, precrec_obj, validation, temp.files, prediction)
   }
- } # end if exists loop
 
+ #} # end if exists loop
+ rm(temp_sdm)
 })}
 
-#mod_eval
+mod_eval
 
 #- - - - - - - - - - - - - - - - - - - 
 ## Save ####
 
 write.csv(mod_eval, file=paste0(here::here(), "/results/", Taxon_name,"/ModelEvaluation_", Taxon_name, "_", spID, ".csv"), row.names = F)
-
+rm(mod_eval)
 })}
 
 #- - - - - - - - - - - - - - - - - - - 
 ## Combine all evaluation tables ####
 
-mod_eval <- data.frame(species = NA, # name of the species (more important for later)
-                       roc = NA, # AUC roc
-                       prg = NA, # AUC prg
-                       cor = NA, # correlation between predicted & true values
-                       tss = NA, # True Skills Statisti
-                       kappa = NA, # Cohen's kappa
-                       thres.maxTSS = NA, # Threshold (presence vs. absence) at max. TSS (max(se+sp))
-                       model = NA, # name of the SDM algorithm
-                       time_model = NA, # mean computational time for model training
-                       time_predict = NA, # mean computational time for prediction
-                       bg= NA, # background dataset used
-                       no.runs = NA, #how many background data runs to we have
-                       n_vali_presence = NA, # number of validation presence records
-                       n_vali_absence = NA) # number of validation absence records
+mod_eval <- mod_eval_template
 
 temp_files <- list.files(paste0(here::here(), "/results/", Taxon_name))
 temp_files <- temp_files[stringr::str_detect(temp_files, "ModelEvaluation")]
