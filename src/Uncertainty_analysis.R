019b672707ed4c067b2f56a8cfcacbf73ac158a6
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-05-20 16:41:21 +0200

    Fix uncertainty for all species

--- src/Uncertainty_analysis.R
+++ src/Uncertainty_analysis.R
@@ -5,11 +5,15 @@
 #                                           #
 #- - - - - - - - - - - - - - - - - - - - - -#
 
-# load species-specific models
-load(file=paste0(here::here(), "/results/_Maps/SDM_Predictions_", Taxon_name, "_", spID, ".RData")) #model_list
-
 # load environmental space as data frame
 load(paste0(here::here(),"/results/EnvPredictor_2km_df_normalized.RData")) #Env_norm_df
+
+
+for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 5,]$SpeciesID)){ try({
+print(paste0("Species: ", spID))
+
+# load species-specific models
+load(file=paste0(here::here(), "/results/_Maps/SDM_Predictions_", Taxon_name, "_", spID, ".RData")) #model_list
 
 # create empty data frame
 uncertain_df <- Env_norm_df %>% dplyr::select(x, y)
@@ -35,15 +39,41 @@ uncertain_df$SD <- apply(uncertain_df %>% dplyr::select(-x, -y), 1, sd, na.rm = 
 
 head(uncertain_df)
 
-# view uncertainty in map 
-#png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"), width=1000, height=1000)
-ggplot(data=uncertain_df, aes(x=x, y=y, fill=SD))+
-  geom_tile()+
-  ggtitle("SD between model predictions")+
-  scale_fill_viridis_c()+
-  theme_bw()+
-  theme(axis.title = element_blank(), legend.title = element_blank(),
-        legend.position = c(0.1,0.4))
-dev.off()
+# save species' uncertainty map
+save(uncertain_df, file=paste0(here::here(), "/results/_Maps/SDM_Uncertainty_", Taxon_name, "_", spID, ".RData"))
 
+# view uncertainty in map 
+# png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"), width=1000, height=1000)
+# ggplot(data=uncertain_df, aes(x=x, y=y, fill=SD))+
+#   geom_tile()+
+#   ggtitle("SD between model predictions")+
+#   scale_fill_viridis_c()+
+#   theme_bw()+
+#   theme(axis.title = element_blank(), legend.title = element_blank(),
+#         legend.position = c(0.1,0.4))
+# dev.off()
+
+})}
+
+#- - - - - - - - - - - - - - - - - - - - - -
+## Stack species uncertainty maps ####
+
+# create empty data frame
+species_stack <- Env_norm_df %>% dplyr::select(x, y)
+
+# for loop through all species
+for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 5,]$SpeciesID)){ try({
+
+  load(file=paste0(here::here(), "/results/_Maps/SDM_Uncertainty_", Taxon_name, "_", spID, ".RData")) #uncertain_df
+  uncertain_df[,paste0("SD.", spID)] <- uncertain_df$SD
+
+  # add species dataframe to stacked dataframe
+  species_stack <- species_stack %>% full_join(uncertain_df %>% dplyr::select(x,y,paste0("SD.", spID)), by=c("x","y"))
+  
+  print(paste0("Added binary prediction of ", spID, " to the species stack"))
+  
+  rm(uncertain_df)
+}, silent=T)}
+
+head(species_stack)
 
