0aff6511c5e983b7b75a202f4b0d50c1a7e9eddb
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-05-24 15:44:29 +0200

    Try fix varImp [needs more work]

--- src/Variable_importance.R
+++ src/Variable_importance.R
@@ -6,12 +6,12 @@
 #- - - - - - - - - - - - - - - - - - - - - -#
 
 ## load models ####
-load(file=paste0(here::here(), "/sdm/SDM_Models_", spID, ".RData")) #SDMs
+#load(file=paste0(here::here(), "/sdm/SDM_Models_", spID, ".RData")) #SDMs
 
 #- - - - - - - - - - - - - - - - - - - - - -
 ## Calculate variable importance (VI) ####
 # create result data frame
-var_imp_temp <- data.frame("Predictor"= c("Aridity", "MAP", "MAP_Seas", "MAT", 
+var_imp_template <- data.frame("Predictor"= c("Aridity", "MAP", "MAP_Seas", "MAT", 
                                      "MAT_Seas", "Snow", "Agriculture", "Dist_Urban",
                                      "Forest_Coni", "Forest_Deci", "NDVI", 
                                      "Pastures", "Pop_Dens", "Shrubland", "Aspect",
@@ -21,6 +21,9 @@ var_imp_temp <- data.frame("Predictor"= c("Aridity", "MAP", "MAP_Seas", "MAT",
 
 sdm_names <- c("gm", "lm1", "lm_subset", "lasso", "ridge", "mars", "maxent", "maxnet", "brt", 
                "brt2", "xgb", "svm", "rf", "rf2", "rf_downsample", "biomod", "ensm")
+
+# Calculate the number of cores
+no.cores <-  parallel::detectCores()/2 
 
 registerDoParallel(no.cores)
 foreach(spID = unique(speciesNames[speciesNames$NumCells_2km >= 5,]$SpeciesID), 
@@ -70,14 +73,22 @@ for(i in 1:length(sdm_names)){ try({
   
   rm(temp_vi, temp_sdm)
   
-}}, silent=F)}
+}, silent=F)}
 
 print("Error in ensm_pred is not an issue")
 
 #- - - - - - - - - - - - - - - - - - - - - -
 ## Ensemble ####
 # average variable importances of individual models
-var_imp$ensemble <- round(rowMeans(var_imp[,c("gm", "lasso", "brt2", "rf2", "maxent")], na.rm=T),3)
+temp_varImp <- var_imp_template
+try(temp_varImp <- temp_varImp %>% full_join(var_imp$gm), silent=T)
+try(temp_varImp <- temp_varImp %>% full_join(var_imp$lasso), silent=T)
+try(temp_varImp <- temp_varImp %>% full_join(var_imp$brt2), silent=T)
+try(temp_varImp <- temp_varImp %>% full_join(var_imp$rf2), silent=T)
+try(temp_varImp <- temp_varImp %>% full_join(var_imp$maxent), silent=T)
+
+try({var_imp$ensemble <- round(rowMeans(temp_varImp, na.rm=T),3)
+rm(temp_varImp)
 
 var_imp
 
