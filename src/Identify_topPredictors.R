ef8a00ff8ef46ca4adf44f850cb65b206c0d86ee
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-06-16 19:48:28 +0200

    Create Identify_topPredictors.R

---
+++ src/Identify_topPredictors.R
@@ -0,0 +1,377 @@
+#- - - - - - - - - - - - - - - - - - - - - -#
+#                                           #
+#    Identify top predictors with MaxEnt    #
+#          author: Romy Zeiss               #
+#                                           #
+#- - - - - - - - - - - - - - - - - - - - - -#
+
+#setwd("~/share/groups/eie/==PERSONAL/RZ_SoilBON/SoilBiodiversity_RStudio")
+
+options(java.parameters = c("-XX:+UseConcMarkSweepGC", "-Xmx8g")) # expand Java memory
+#Sys.setenv(JAVA_HOME='\\home\\idivrz90seqi\\share\\groups\\eie\\==PERSONAL\\RZ_SoilBON\\SoilBiodiversity_RStudio')
+
+gc()
+library(tidyverse)
+library(here)
+
+library(raster)
+
+library(biomod2) # also to create pseudo-absences
+
+library(dismo) # for MaxEnt and BRT
+# download maxent.jar 3.3.3k, and place the file in the
+# desired folder
+# utils::download.file(url = "https://raw.githubusercontent.com/mrmaxent/Maxent/master/ArchivedReleases/3.3.3k/maxent.jar", 
+#     destfile = paste0(system.file("java", package = "dismo"), 
+#         "/maxent.jar"), mode = "wb")  ## wb for binary file, otherwise maxent.jar can not execute
+
+library(rJava)
+
+# for model performance:
+library(precrec)
+# devtools::install_github("meeliskull/prg/R_package/prg")
+library(prg)
+library(ggpubr)
+library (ROCR)
+library(sdm) # to calculate kappa
+
+library(gridExtra) #to plot multiple panels
+
+
+#write("TMPDIR = 'D:/00_datasets/Trash'", file=file.path(Sys.getenv('R_USER'), '.Renviron'))
+
+# change temporary directory for files
+#raster::rasterOptions(tmpdir = "D:/00_datasets/Trash")
+
+#- - - - - - - - - - - - - - - - - - - - -
+Taxon_name <- "Crassiclitellata"
+speciesNames <- read.csv(file=paste0("./results/Species_list_", Taxon_name, ".csv"))
+
+corMatPearson <- as.matrix(read.csv(file=paste0("./results/corMatPearson_predictors.csv")))
+dimnames(corMatPearson)[[1]] <- dimnames(corMatPearson)[[2]]
+# based on Valavi et al. 2021: Pearson 0.8
+env_exclude <- caret::findCorrelation(corMatPearson, cutoff = 0.8, names=TRUE)
+covarsNames <- dimnames(corMatPearson)[[1]][!(dimnames(corMatPearson)[[1]] %in% env_exclude)]
+covarsNames <- covarsNames[covarsNames != "x" & covarsNames != "y"]
+# exclude based on VIF
+env_vif <- read.csv(file=paste0("./results/VIF_predictors.csv"))
+env_exclude <- env_vif %>% filter(is.na(VIF)) %>% dplyr::select(Variables) %>% as.character()
+covarsNames <- covarsNames[!(covarsNames %in% env_exclude)]
+# excluded:
+print("=== We excluded the following variables based on VIF and Pearson correlation: ===")
+setdiff(env_vif$Variables, covarsNames)
+
+# final predictor variables
+print("=== And we kept the following, final predictor variables: ===")
+covarsNames
+
+#- - - - - - - - - - - - - - - - - - - - -
+# note: we will load the datasets before each individual model
+
+# load environmental variables (for projections)
+Env_norm <- raster::stack(paste0("./results/EnvPredictor_2km_normalized.grd"))
+#Env_norm <- stack(Env_norm)
+
+# as dataframe
+load(paste0("./results/EnvPredictor_2km_df_normalized.RData")) #Env_norm_df
+
+# define formula for GLM (and biomod)
+form <- paste0("occ ~ ", paste0(paste0("s(", covarsNames, ")"), collapse=" + "))
+
+
+# Calculate the number of cores
+no.cores <-  parallel::detectCores()/2 
+
+- - - - - - - - - - - - - - - - - - - - -
+## Prepare model input ####
+
+for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 10,]$SpeciesID)) { try({
+  
+  mySpeciesOcc <- read.csv(file=paste0("./results/Occurrence_rasterized_2km_", Taxon_name, ".csv"))
+  myResp <- as.numeric(mySpeciesOcc[,spID])
+  
+  # get NAs id
+  na.id <- which(is.na(myResp))
+  # remove NAs to enforce PA sampling to be done on explanatory rasters
+  myResp <- myResp[-na.id]
+  
+  myRespCoord <- mySpeciesOcc[-na.id,c('x','y')]
+  
+  myData <- biomod2::BIOMOD_FormatingData(resp.var = myResp,
+                                                expl.var = Env_norm,
+                                                resp.xy = myRespCoord,
+                                                resp.name = spID,
+                                                PA.nb.rep = 1,
+                                                PA.nb.absences = 10000,
+                                                PA.strategy = "random")
+  
+  myData <- cbind(myData@data.species, myData@coord, myData@data.env.var)
+  myData$SpeciesID <- spID
+  myData <- myData %>% rename("occ" = "myData@data.species")
+  myData[is.na(myData$occ),"occ"] <- 0
+  
+  random.rows <- sample(1:nrow(myData), nrow(myData))
+  
+  validation <- myData[random.rows[1:round(0.2*nrow(myData))], 
+                         c("x","y", "SpeciesID", "occ", covarsNames[covarsNames %in% colnames(myData)])]
+  
+  training <- myData[random.rows[round(0.2*nrow(myData)):nrow(myData)],]
+  
+  # subset uncorrelated covariates
+  training <- training[, c("occ", covarsNames[covarsNames %in% colnames(myData)])]
+  
+  # save all datasets
+  save(training, file=paste0("./results/", Taxon_name, "/_TopPredictor/MaxentData_train_", Taxon_name,"_", spID, ".RData"))
+  save(validation, file=paste0("./results/", Taxon_name, "/_TopPredictor/MaxentData_valid_", Taxon_name,"_", spID, ".RData"))
+  
+})}
+
+
+#- - - - - - - - - - - - - - - - - - - - -
+## Modeling ####
+
+for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 10,]$SpeciesID)[46:68]){ try({
+ 
+  modelName <- "MaxentData"
+  
+  # identify and load all relevant data files
+  temp.files <- list.files(path = paste0("./results/",Taxon_name, "/_TopPredictor"), 
+                           pattern = paste0(modelName, "_[[:graph:]]*_", spID), full.name = T)
+  lapply(temp.files, load, .GlobalEnv)
+  
+  # "We used five different regularization multipliers (0.5, 1, 2, 3 and 4)
+  # in combination with different features (L, LQ, H, LQH, LQHP) to find the 
+  # best parameters that maximizes the average AUC-ROC in CV."
+  
+  # function for simultaneous tuning of MaxEnt regularization multiplier and features
+  maxent_param <- function(data, y = "occ", k = 5, folds = NULL, filepath){
+    require(dismo)
+    require(caret)
+    require(precrec)
+    if(is.null(folds)){
+      # generate balanced CV folds
+      folds <- caret::createFolds(y = as.factor(data$occ), k = k)
+    }
+    names(data)[which(names(data) == y)] <- "occ"
+    
+    # regularization multipliers
+    ms <- c(0.5, 1, 2, 3, 4)
+    grid <- expand.grid(
+      regmult = paste0("betamultiplier=", ms),
+      
+      # features
+      features = list(
+        c("noautofeature", "nothreshold"), # LQHP
+        c("noautofeature", "nothreshold", "noproduct"), # LQH
+        c("noautofeature", "nothreshold", "nohinge", "noproduct"), # LQ
+        c("noautofeature", "nothreshold", "nolinear", "noquadratic", "noproduct"), # H
+        c("noautofeature", "nothreshold", "noquadratic", "nohinge", "noproduct")), # L
+      stringsAsFactors = FALSE
+    )
+    AUCs <- c()
+    for(n in seq_along(grid[,1])){
+      full_pred <- data.frame()
+      for(i in seq_len(length(folds))){
+        trainSet <- unlist(folds[-i])
+        testSet <- unlist(folds[i])
+        if(inherits(try(
+          maxmod <- dismo::maxent(x = data[trainSet, covarsNames],
+                                  p = data$occ[trainSet],
+                                  removeDuplicates = FALSE,
+                                  path = filepath,
+                                  args = as.character(unlist(grid[n, ])))
+        ), "try-error")){
+          next
+        }
+        modpred <- dismo::predict(maxmod, data[testSet, covarsNames], type = c("cloglog")) #, args = "outputformat=cloglog"
+        pred_df <- data.frame(score = modpred, label = data$occ[testSet])
+        full_pred <- rbind(full_pred, pred_df)
+      }
+      AUCs[n] <- precrec::auc(precrec::evalmod(scores = full_pred$score,
+                                               labels = full_pred$label))[1,4]
+    }
+    best_param <- as.character(unlist(grid[which.max(AUCs), ]))
+    return(best_param)
+  }
+  
+  ## now use the function to tune MaxEnt
+  # number of folds
+  nfolds <- ifelse(sum(as.numeric(training$occ)) < 10, 2, 5)
+  
+  tmp <- proc.time()[3]
+  set.seed(32639)
+  
+  # tune MaxEnt parameters
+  param_optim <- maxent_param(data = training,
+                              k = nfolds,
+                              filepath = paste0("results/", Taxon_name, "/_TopPredictor/maxent_files"))
+  
+  ## fit a maxent model with the tuned parameters
+  maxent <- dismo::maxent(x = training[, covarsNames],
+                          p = training$occ,
+                          removeDuplicates = FALSE, #remove occurrences that fall into same grid cell (not necessary)
+                          #path = paste0("results/", Taxon_name, "/maxent_files"), #wanna save files?
+                          args = param_optim)
+  
+  #temp_validation <- dismo::predict(maxent, validation[,colnames(validation) %in% covarsNames], type="response")
+  temp_validation <- dismo::predict(maxent, validation[,colnames(validation) %in% covarsNames], type = c("cloglog"))
+  temp_validation <- as.numeric(temp_validation)
+  names(temp_validation) <- rownames(validation[,colnames(validation) %in% covarsNames]) #add site names
+  #head(temp_validation)
+  
+  temp_model_time <- proc.time()[3] - tmp
+  
+  
+  tmp <- proc.time()[3]
+  # create raster layer of predictions for whole environmental space
+  #temp_prediction <- raster::predict(Env_norm, maxent)
+  #temp_prediction <- data.frame(raster::rasterToPoints(temp_prediction))
+  gc()
+  #temp_prediction <- dismo::predict(maxent, Env_norm_df %>% dplyr::select(-x, -y)) # Java out of memory
+  temp_prediction <- dismo::predict(maxent, Env_norm_df[,colnames(Env_norm_df) %in% covarsNames], type = c("cloglog"))
+  temp_prediction <- as.numeric(temp_prediction)
+  names(temp_prediction) <- rownames(Env_norm_df[complete.cases(Env_norm_df[,colnames(Env_norm_df) %in% covarsNames]),]) #add site names
+  temp_prediction <- as.data.frame(temp_prediction)
+  temp_prediction$x <- Env_norm_df[complete.cases(Env_norm_df[,colnames(Env_norm_df) %in% covarsNames]),]$x
+  temp_prediction$y <- Env_norm_df[complete.cases(Env_norm_df[,colnames(Env_norm_df) %in% covarsNames]),]$y
+  colnames(temp_prediction)[1] <- "layer"
+  
+  temp_runs <- 1
+  
+  temp_predict_time <- proc.time()[3] - tmp
+  
+  # varImp
+  temp_varImp <- as.data.frame(maxent@results[str_detect(rownames(maxent@results),"permutation.importance"),])
+  # Note: permutation importance = determine the importance of predictors 
+  # calculated by permuting values of each predictor &  resulting reduction
+  # in training AUC: large reduction = model is influenced by that predictor. 
+  
+  # extract predictor names
+  temp_varImp$Predictor <- stringr::str_split_fixed(rownames(temp_varImp), "[.]perm", 2)[,1]
+  colnames(temp_varImp) <- c("maxent", "Predictor")  
+  # plot variable importance
+  #plot(maxent)   
+  # plot response curve
+  #response(maxent)  
+  
+  maxent_list <- list(bg_data=modelName, time_model=temp_model_time, time_predict=temp_predict_time, runs=temp_runs, validation=temp_validation, prediction=temp_prediction, varImp=temp_varImp)
+  save(maxent_list, file=paste0("./results/", Taxon_name, "/_TopPredictor/SDM_maxent_", spID, ".RData"))
+  
+  rm(maxent, maxent_list, param_optim, temp_model_time, temp_predict_time, temp_runs, temp_validation, temp_prediction, temp_varImp)
+  gc()
+  
+  rm(training, validation)
+
+})}
+
+
+#- - - - - - - - - - - - - - - - - - - - - -
+## Calculate variable importance (VI) ####
+
+# create result data frame
+var_imp <- data.frame("Predictor"= c("Aridity", "MAP", "MAP_Seas", "MAT", 
+                                              "MAT_Seas", "Snow", "Agriculture", "Dist_Urban",
+                                              "Forest_Coni", "Forest_Deci", "NDVI", 
+                                              "Pastures", "Pop_Dens", "Shrubland", "Aspect",
+                                              "Dist_Coast", "Dist_River", "Elev", 
+                                              "Slope", "CEC", "Clay.Silt", "Cu", "Hg",
+                                              "Moisture", "N", "P", "pH", "SOC", "SoilT"),
+                      "maxent"=NA, "Species"=NA)
+
+for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 10,]$SpeciesID)){ try({
+  
+  print("=====================================")
+  print(spID)
+  
+  temp_sdm <- get(load(file=paste0("./results/", Taxon_name, "/_TopPredictor/SDM_maxent_", spID, ".RData")))
+
+  temp_vi <- temp_sdm[["varImp"]]
+  
+  # harmonizes predictor column structure
+  temp_vi$Predictor <- as.character(temp_vi$Predictor)
+
+  # round variable importance to 3 decimals
+  temp_vi[,"maxent"] <- round(temp_vi[,"maxent"], 3)
+      
+  # add species name
+  temp_vi$Species <- spID
+  
+  var_imp <- rbind(var_imp, temp_vi)
+
+  rm(temp_vi, temp_sdm, temp_varImp)
+    
+  #var_imp
+          
+})}
+
+rownames(var_imp) <- 1:nrow(var_imp)
+var_imp <- var_imp[!is.na(var_imp$Species),] %>% unique()
+
+var_imp
+str(var_imp)
+
+## Save ####
+write_csv(var_imp, file=paste0("./results/Variable_importance_MaxEnt_", Taxon_name, ".csv"))
+ 
+
+#- - - - - - - - - - - - - - - - - -
+## Vizualize and get top 10 ####
+var_imp <- read.csv(file=paste0("./results/Variable_importance_MaxEnt_", Taxon_name, ".csv"))
+var_imp
+
+# load predictor table to get classification of variables
+# load the predictor table containing the individual file names
+pred_tab <- readr::read_csv(file=paste0("./Env_Predictors_table.csv"))
+
+# transform to long format and add variable categories
+var_imp <- var_imp %>%
+  left_join(pred_tab %>% dplyr::select(Predictor, Category), by="Predictor")
+
+# add category for clay.silt
+var_imp[var_imp$Predictor=="Clay.Silt","Category"] <- "Soil"
+
+# plot VIF
+plotVarImp <- ggplot(data=var_imp, aes(x=maxent, y=Predictor, fill=Category))+
+  geom_boxplot(cex=0.2, outlier.size=0.2)+
+  xlab("Variable importance (Permutation importances)")+
+  theme_bw()+
+  theme(axis.text.y = element_text(size = 3))
+plotVarImp
+
+pdf(paste0(here::here(), "/figures/VariableImportance_MaxEnt_", Taxon_name, ".pdf")); plotVarImp; dev.off()
+
+# plot barplot with top 10
+plotTopVI <- var_imp %>% dplyr::select(maxent, Predictor, Category) %>%
+  group_by(Predictor, Category) %>% summarize_all(mean, na.rm=T) %>% arrange(desc(maxent)) %>%
+  ggplot(aes(x=maxent, y=reorder(Predictor, maxent), fill=Category)) + 
+  geom_bar(stat="identity") + geom_line(y=length(covarsNames)-9.5)+
+  geom_text(aes(label=round(maxent,3)), position=position_dodge(width=0.5), vjust=0.5, hjust=1.1, cex=3)
+plotTopVI
+
+# pdf(paste0(here::here(), "/figures/VariableImportance_MaxEnt_top10_", Taxon_name, ".pdf")); plotTopVI; dev.off()
+
+# plot maps
+temp_files <- list.files(paste0("./results/", Taxon_name, "/_TopPredictor"))
+temp_files <- temp_files[stringr::str_detect(temp_files, "SDM_maxent_[:graph:]*.RData")]
+
+plots <- lapply(c(1:length(temp_files)), function(m) {try({
+  print(temp_files[m]); print(m)
+  temp_pred <- get(load(file=paste0("./results/", Taxon_name, "/_TopPredictor/", temp_files[m])))[["prediction"]]
+  #print(m)
+  spID <- substr(temp_files[m], 12, 21)
+  ggplot(data=temp_pred, aes(x=x, y=y, fill=layer))+
+    geom_tile()+
+    ggtitle(paste0("MaxEnt for ", spID))+
+    scale_fill_viridis_c(limits = c(0,1))+
+    theme_bw()+
+    theme(axis.title = element_blank(), legend.title = element_blank(),
+          legend.position = "right")
+})})
+
+lapply(plots, class) # if error, remove that species
+
+#pdf(file=paste0(here::here(), "/figures/DistributionMaps_", Taxon_name, "_", spID, ".pdf"))
+png(file=paste0("./figures/DistributionMaps_", Taxon_name, "_MaxEnt.png"),width=3000, height=3000)
+do.call(gridExtra::grid.arrange, plots)
+dev.off()
+
