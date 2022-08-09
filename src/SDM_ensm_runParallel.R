404a30b160b02f456cf13a9c81f39035c4aef0ce
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-08-09 11:50:52 +0200

    Remove evaluation from biomod_list and fix species stack

--- src/SDM_ensm_runParallel.R
+++ src/SDM_ensm_runParallel.R
@@ -348,7 +348,7 @@ Env_norm <- raster::stack(paste0(here::here(), "/results/EnvPredictor_5km_normal
 load(paste0(here::here(),"/results/EnvPredictor_5km_df_normalized.RData")) #Env_norm_df
 
 registerDoParallel(no.cores)
-foreach(spID = speciesSub, 
+foreach(spID = speciesSub,
         .export = c("Env_norm", "Env_norm_df", "form"),
         .packages = c("tidyverse","biomod2")) %dopar% { try({   
           
@@ -422,8 +422,8 @@ foreach(spID = speciesSub,
           
           temp_runs <- 1
           
-          biomod_list <- list(time_predict=temp_predict_time, validation=myBiomodModelEval, prediction=temp_prediction, varImp=temp_varImp, evaluation=myBiomodModelEval)
-          save(biomod_list, file=paste0("./SDMs/SDM_biomod_", spID, ".RData"))
+          biomod_list <- list(time_predict=temp_predict_time, validation=myBiomodModelEval, prediction=temp_prediction, varImp=temp_varImp)
+          save(biomod_list, file=paste0("./_SDMs/SDM_biomod_", spID, ".RData"))
           
           rm(biomod_list, temp_predict_time, temp_runs, temp_prediction, temp_varImp, myBiomodEnProj, myBiomodProj, myBiomodModelEval, myEnProjDF, myBiomodModelOut, myBiomodEM)
           
@@ -439,10 +439,12 @@ stopImplicitCluster()
 setwd(here::here())
 
 registerDoParallel(no.cores)
-foreach(spID = speciesSub, 
+foreach(spID = speciesSub,
         .export = c("Env_norm", "Env_norm_df", "form"),
         .packages = c("tidyverse","biomod2")) %dopar% { try({ 
-          
+
+#for(spID in speciesSub){ try({ 
+
           # list files in species-specific BIOMOD folder
           temp_files <- list.files(paste0(here::here(), "/results/", Taxon_name, "/", stringr::str_replace(spID, "_", ".")), full.names = TRUE)
           
@@ -511,13 +513,13 @@ foreach(spID = speciesSub,
               temp_prediction$layer <- temp_prediction$layer / 1000
               
               setwd(here::here())
-              save(temp_prediction, file=paste0(here::here(), "/results/_Maps/SDM_2041-2070_", no_future, "_", subclim, "_biomod_", spID,  ".RData")) 
+              save(temp_prediction, file=paste0(here::here(), "/results/_SDMs/SDM_2041-2070_", no_future, "_", subclim, "_biomod_", spID,  ".RData")) 
               rm(temp_prediction, temp_Env_sub, myBiomodEnProj, myBiomodProj)
             }
           }
                
 })}
-stopImplicitCluster()
+#stopImplicitCluster()
 
 
 
@@ -531,26 +533,26 @@ species_stack <- Env_norm_df %>% dplyr::select(x, y)
 for(spID in speciesSub){ try({
 
   ## Load probability maps 
-  load(file=paste0(here::here(), "/SDMs/SDM_biomod_", spID, ".RData")) #biomod_list
-  
+  load(file=paste0(here::here(), "/results/", Taxon_name, "/_SDMs/SDM_biomod_", spID, ".RData")) #biomod_list
+  best_pred <- biomod_list$prediction
   
   print(paste0(spID, " successfully loaded."))
   
   ## Transform to binary maps ####
   
-  # extract threshold to define presence/absence
-  temp_thresh <- mod_eval[mod_eval$model==temp_model,"thres.maxTSS"]
+  # extract threshold to define presence/absence: TSS [row 2]
+  temp_thresh <- biomod_list$validation[2,str_detect(colnames(biomod_list$validation), "EMcaByTSS_mergedAlgo_mergedRun_mergedData.Cutoff")]/1000
   if(is.na(temp_thresh)) temp_tresh <- 0.9
   
   # change to binary
   best_pred[best_pred$layer>=temp_thresh & !is.na(best_pred$layer), "layer"] <- 1
   best_pred[best_pred$layer<temp_thresh & !is.na(best_pred$layer), "layer"] <- 0
   
-  best_pred[,paste0(spID,"_", temp_model)] <- best_pred$layer
-  best_pred <- best_pred[,c("x","y",paste0(spID,"_", temp_model))]
+  best_pred[,paste0(spID,"_current")] <- best_pred$layer
+  best_pred <- best_pred[,c("x","y",paste0(spID,"_current"))]
   
   # save binary
-  save(best_pred, file=paste0(here::here(), "/results/_Maps/SDM_bestPrediction_binary_", Taxon_name, "_", spID, ".RData"))
+  save(best_pred, file=paste0(here::here(), "/results/", Taxon_name, "/_SDMs/SDM_bestPrediction_binary_", Taxon_name, "_", spID, ".RData"))
   
   print(paste0("Saved binary prediction of ", spID))
   
@@ -562,7 +564,7 @@ for(spID in speciesSub){ try({
   
   print(paste0("Added binary prediction of ", spID, " to the species stack"))
   
-  rm(temp_thresh, best_pred, temp_model)
+  rm(temp_thresh, best_pred)
 }, silent=T)}  
 
 head(species_stack)
@@ -614,8 +616,122 @@ dev.off()
 
 while (!is.null(dev.list()))  dev.off()
 
+#- - - - - - - - - - - - - - - - - - - - - -
+## Create future maps and calculate richness ####
+for(no_future in scenarioNames){
   
+  # one loop per future climate subset, one with both future, each one with only 1 future and 1 current climate
+  for(subclim in c("TP", "T", "P")){
+    
+    if(subclim=="TP"){
+      temp_Env_sub <- temp_Env_df[,c("x", "y", colnames(temp_Env_df)[colnames(temp_Env_df) %in% covarsNames])]
+    }
+    
+    if(subclim=="T"){
+      temp_Env_sub <- temp_Env_df[,c("x", "y", colnames(temp_Env_df)[colnames(temp_Env_df) %in% covarsNames])]
+      temp_Env_sub$MAP_Seas <- Env_norm_df$MAP_Seas
+    }
+    
+    if(subclim=="P"){
+      temp_Env_sub <- temp_Env_df[,c("x", "y", colnames(temp_Env_df)[colnames(temp_Env_df) %in% covarsNames])]
+      temp_Env_sub$MAT <- Env_norm_df$MAT
+    }
+        
+    #- - - - - - - - - - - - - - - - - - - - - -
+    # create empty data frame
+    species_stack <- Env_norm_df %>% dplyr::select(x, y)
+    
+    # for loop through all species
+    for(spID in speciesSub){ try({
+      
+      ## Load probability maps 
+      load(file=paste0(here::here(), "/results/_SDMs/SDM_2041-2070_", no_future, "_", subclim, "_biomod_", spID,  ".RData")) #biomod_list
+      best_pred <- biomod_list$prediction
+      
+      print(paste0(spID, " successfully loaded."))
+      
+      ## Transform to binary maps ####
+      
+      # extract threshold to define presence/absence
+      temp_thresh <- biomod_list$validation[2,str_detect(colnames(biomod_list$validation), "EMcaByTSS_mergedAlgo_mergedRun_mergedData.Cutoff")]/1000
+      if(is.na(temp_thresh)) temp_tresh <- 0.9
+      
+      # change to binary
+      best_pred[best_pred$layer>=temp_thresh & !is.na(best_pred$layer), "layer"] <- 1
+      best_pred[best_pred$layer<temp_thresh & !is.na(best_pred$layer), "layer"] <- 0
+      
+      best_pred[,paste0(spID,"_", temp_model)] <- best_pred$layer
+      best_pred <- best_pred[,c("x","y",paste0(spID,"_", temp_model))]
+      
+      # save binary
+      save(best_pred, file=paste0(here::here(), "/results/_SDMs/SDM_bestPrediction_binary_2041-2070_", no_future, "_", subclim, "_biomod_", spID,  ".RData"))
+      
+      print(paste0("Saved binary prediction of ", spID))
+      
+      #- - - - - - - - - - - - - - - - - - - - - -
+      ## Stack species binary maps ####
+      
+      # add species dataframe to stacked dataframe
+      species_stack <- species_stack %>% full_join(best_pred, by=c("x","y"))
+      
+      print(paste0("Added binary prediction of ", spID, " to the species stack"))
+      
+      rm(temp_thresh, best_pred, temp_model)
+    }, silent=T)}  
+    
+    head(species_stack)
+    
+    
+    #- - - - - - - - - - - - - - - - - - - - - -
+    ## Calculate richness ####
+    species_stack$Richness <- rowSums(species_stack %>% dplyr::select(-x, -y), na.rm=T)
+    
+    #- - - - - - - - - - - - - - - - - - - - - -
+    ## Save species stack ####
+    save(species_stack, file=paste0(here::here(), "/results/_Maps/SDM_stack_bestPrediction_binary_", "2041-2070_", no_future, "_", subclim, ".RData"))
+    
+    
+    #- - - - - - - - - - - - - - - - - - - - - -
+    ## View individual binary maps and species stack ####
+    # species richness
+    png(file=paste0(here::here(), "/figures/SpeciesRichness_", "2041-2070_", no_future, "_", subclim, "_", Taxon_name, ".png"),width=1000, height=1000)
+    ggplot(data=species_stack, aes(x=x, y=y, fill=Richness))+
+      geom_tile()+
+      ggtitle("Species richness (number of species)")+
+      scale_fill_viridis_c()+
+      theme_bw()+
+      theme(axis.title = element_blank(), legend.title = element_blank(),
+            legend.position = c(0.1,0.4))
+    dev.off()
+    
+    while (!is.null(dev.list()))  dev.off()
+    
+    
+    # map binary species distributions
+    plots <- lapply(3:(ncol(species_stack)-1), function(s) {try({
+      print(s-2)
+      ggplot(data=species_stack, aes(x=x, y=y, fill=as.factor(species_stack[,s])))+
+        geom_tile()+
+        ggtitle(colnames(species_stack)[s])+
+        scale_fill_manual(values=c("1"="#fde725","0"="#440154","NA"="lightgrey"))+
+        theme_bw()+
+        theme(axis.title = element_blank(), legend.title = element_blank(),
+              legend.position = c(0.1,0.4))
+    })
+    })
+    
+    require(gridExtra)
+    #pdf(file=paste0(here::here(), "/figures/DistributionMap_bestBinary_2041-2070_", no_future, "_", subclim, "_", Taxon_name, ".pdf"))
+    png(file=paste0(here::here(), "/figures/DistributionMap_bestBinary_2041-2070_", no_future, "_", subclim, "_", Taxon_name, ".png"),width=3000, height=3000)
+    do.call(grid.arrange, plots)
+    dev.off()
+    
+    while (!is.null(dev.list()))  dev.off()
+  }
+}
   
+
+
 #- - - - - - - - - - - - - - - - - - - - -
 ## TRASH?
 #- - - - - - - - - - - - - - - - - - - - -
