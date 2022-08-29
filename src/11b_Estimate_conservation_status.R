92eb247145e8ff76702e2984b2ae05725633d90a
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-08-29 16:09:31 +0200

    Fix and add protection status (mean and plot order)

--- src/11b_Estimate_conservation_status.R
+++ src/11b_Estimate_conservation_status.R
@@ -30,22 +30,29 @@ for(sp in unique(speciesNames$SpeciesID)){ try({
 	temp_df$Presence <- temp_df[,colnames(temp_df)[stringr::str_detect(colnames(temp_df), sp)]]
 	temp_df <- temp_df[,c(names(protect_df %>% dplyr::select(-x, -y)), "Presence")]
 
-	if(length(unique(stringr::str_detect(colnames(df), sp)))==1) next
-
 	# keep only presence rows
 	temp_df <- temp_df[temp_df[,"Presence"]==1 & !is.na(temp_df[,"Presence"]),]
 
 	# calculate sum of all columns (will give you coverage)
 	temp_cover <- data.frame("IUCNcat" = names(temp_df), "sumCell"= as.numeric(colSums(temp_df)))
+	temp_cover <- temp_cover %>%
+	  add_row("IUCNcat"="Unprotected", "sumCell"=temp_cover[temp_cover$IUCNcat=="Presence","sumCell"]-sum(colSums(temp_df %>% dplyr::select(-Presence), na.rm=T)))
 	temp_cover$SpeciesID <- sp
 	temp_cover$coverage <- round(temp_cover$sumCell / sum(temp_df[,"Presence"], na.rm=T),4)
 
 	cover_df <- rbind(cover_df, temp_cover)
 	rm(temp_cover, temp_df)
+	
+	print(paste0("Species ", sp, " is ready."))
+	
 }, silent=TRUE)}
 
 cover_df$coverage_km2 <- round(cover_df$sumCell * 5, 2)
 
+cover_df <- cover_df %>% arrange(SpeciesID, IUCNcat) %>% filter(!is.na(coverage))
+
+cover_df <- rbind(cover_df, 
+                  cbind(cover_df %>% group_by(IUCNcat) %>% dplyr::select(-SpeciesID) %>% summarize_all(mean), "SpeciesID"="_Mean"))
 cover_df
 
 write.csv(cover_df, file=paste0(here::here(), "/results/ProtectionStatus_", Taxon_name, ".csv"))
@@ -53,23 +60,26 @@ write.csv(cover_df, file=paste0(here::here(), "/results/ProtectionStatus_", Taxo
 
 ## Plotting ####
 cover_df <- read.csv(file=paste0(here::here(), "/results/ProtectionStatus_", Taxon_name, ".csv"))
-cover_df$IUCNcat <- factor(cover_df$IUCNcat, level=c("Presence", "ProtectedAreas","Ia", "Ib", "II", "III", "IV", "V", "VI", "Not.Applicable", "Not.Assigned", "Not.Reported"))
+cover_df$IUCNcat <- factor(cover_df$IUCNcat, level=c("Presence", "ProtectedAreas","Ia", "Ib", "II", "III", "IV", "V", "VI", "Not.Applicable", "Not.Assigned", "Not.Reported", "Unprotected"))
 
 # bar chart of percent area covered by PA per species
-pdf(paste0(here::here(), "/figures/ProtectionStatus_", Taxon_name, ".pdf"))
-ggplot(data=cover_df, aes(x=IUCNcat, y=coverage_km2, group=SpeciesID, fill=IUCNcat))+
-	geom_bar(stat="identity")+
-	facet_wrap(vars(SpeciesID))+ 
+png(paste0(here::here(), "/figures/ProtectionStatus_", Taxon_name, ".png"))
+ggplot(data=cover_df %>% filter(IUCNcat!="Presence"), aes(y=coverage_km2, x=forcats::fct_reorder(.f=SpeciesID, .x=coverage_km2, .fun = mean), fill=IUCNcat))+
+	geom_bar(position="stack", stat="identity")+
 	theme_bw()+
-	theme(legend.position="bottom", axis.text.x=element_text(angle=45, hjust=1, size=6))
+  coord_flip()+
+  scale_fill_viridis_d()+
+  geom_vline(xintercept=9.5, lty=2)+ geom_vline(xintercept=10.5, lty=2)+
+	theme(legend.position="bottom", axis.text.x=element_text(angle=45, hjust=1))
 dev.off()
 
 # bar chart of percent area covered by PA overall
-pdf(paste0(here::here(), "/figures/ProtectionStatus_total_", Taxon_name, ".pdf"))
-ggplot(data=cover_df, aes(x=reorder(IUCNcat, desc(IUCNcat)), y=coverage_km2))+
+png(paste0(here::here(), "/figures/ProtectionStatus_total_", Taxon_name, ".png"))
+ggplot(data=cover_df %>% filter(IUCNcat!="Presence" & SpeciesID!="_Mean"), aes(x=reorder(IUCNcat, desc(IUCNcat)), y=coverage_km2, fill=IUCNcat))+
   geom_boxplot()+
-  geom_jitter(alpha=0.6)+
+  geom_jitter(alpha=0.6, height=0.2)+
   theme_bw()+ coord_flip()+
+  scale_fill_viridis_d()+
   theme(legend.position="none", axis.text.x=element_text(angle=45, hjust=1))
 dev.off()
 
