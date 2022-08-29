1a923b7bac72bfd635ae670c40326f3a9cc8c92f
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-08-29 13:40:48 +0200

    Update Uncertainty_analysis.R

--- src/Uncertainty_analysis.R
+++ src/Uncertainty_analysis.R
@@ -6,36 +6,43 @@
 #- - - - - - - - - - - - - - - - - - - - - -#
 
 # load environmental space as data frame
-load(paste0(here::here(),"/results/EnvPredictor_2km_df_normalized.RData")) #Env_norm_df
+load(paste0(here::here(),"/results/EnvPredictor_5km_df_normalized.RData")) #Env_norm_df
 
-
-for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 5,]$SpeciesID)){ try({
-print(paste0("Species: ", spID))
-
-# load species-specific models
-load(file=paste0(here::here(), "/results/_Maps/SDM_Predictions_", Taxon_name, "_", spID, ".RData")) #model_list
-
-# create empty data frame
 uncertain_df <- Env_norm_df %>% dplyr::select(x, y)
 
-# loop through all models
-for(m in 1:length(model_list)){ try({
-  # load model prediction
-  temp_model <- model_list[[m]]
+for(spID in unique(speciesNames$SpeciesID)){try({
   
-  # rename layer column
-  temp_model[,names(model_list)[m]] <- temp_model$layer
-  temp_model <- temp_model[,c("x","y",names(model_list)[m])]
-  
-  # add species dataframe to stacked dataframe
-  uncertain_df <- uncertain_df %>% full_join(temp_model, by=c("x","y"))
-  
+  print(paste0("Species: ", spID))
+
+  # list files in species-specific BIOMOD folder
+  temp_files <- list.files(paste0(here::here(), "/results/", Taxon_name, "/", stringr::str_replace(spID, "_", "."), "/proj_modeling"), full.names = TRUE)
+          
+  #setwd(paste0(here::here(), "/results/", Taxon_name))
+
+  myBiomodEnProj <- get(load(temp_files[stringr::str_detect(temp_files,"ensemble.RData")]))
+       
+  # save predictions as raster file
+  temp_prediction <- myBiomodEnProj[,1]
+  temp_prediction <- as.numeric(temp_prediction)
+  # add names of grid cell (only for those that have no NA in any layer)
+  names(temp_prediction) <- rownames(Env_norm_df)
+  temp_prediction <- as.data.frame(temp_prediction)
+  temp_prediction$x <- Env_norm_df$x
+  temp_prediction$y <- Env_norm_df$y
+  temp_prediction <- temp_prediction %>% full_join(Env_norm_df %>% dplyr::select(x,y)) %>%
+     rename("layer" = temp_prediction)
+  temp_prediction$layer <- temp_prediction$layer / 1000
+  temp_prediction[,spID] <- temp_prediction$layer
+          
+  # add layer to stack
+  uncertain_df <- full_join(uncertain_df, temp_prediction %>% dplyr::select(x,y, spID))
+ 
 })}
 
-head(uncertain_df)
+uncertain_df$Mean <- rowMeans(uncertain_df %>% dplyr::select(-x, -y), na.rm=T)
 
 # calculate sd of predictions
-uncertain_df$SD <- apply(uncertain_df %>% dplyr::select(-x, -y), 1, sd, na.rm = TRUE)
+uncertain_df$SD <- apply(uncertain_df %>% dplyr::select(-x, -y, -Mean), 1, sd, na.rm = TRUE)
 
 head(uncertain_df)
 
@@ -43,60 +50,46 @@ head(uncertain_df)
 save(uncertain_df, file=paste0(here::here(), "/results/_Maps/SDM_Uncertainty_", Taxon_name, "_", spID, ".RData"))
 
 # view uncertainty in map 
-# png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"), width=1000, height=1000)
-# ggplot(data=uncertain_df, aes(x=x, y=y, fill=SD))+
-#   geom_tile()+
-#   ggtitle("SD between model predictions")+
-#   scale_fill_viridis_c()+
-#   theme_bw()+
-#   theme(axis.title = element_blank(), legend.title = element_blank(),
-#         legend.position = c(0.1,0.4))
-# dev.off()
+world.inp <- map_data("world")
 
-})}
+png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"), width=1000, height=1000)
+ggplot()+
+  geom_map(data = world.inp, map = world.inp, aes(map_id = region), fill = "grey80") +
+    xlim(-23, 60) +
+    ylim(31, 75) +
 
-#- - - - - - - - - - - - - - - - - - - - - -
-## Stack species uncertainty maps ####
-
-# create empty data frame
-species_stack <- Env_norm_df %>% dplyr::select(x, y)
-
-# for loop through all species
-for(spID in unique(speciesNames[speciesNames$NumCells_2km >= 5,]$SpeciesID)){ try({
-
-  load(file=paste0(here::here(), "/results/_Maps/SDM_Uncertainty_", Taxon_name, "_", spID, ".RData")) #uncertain_df
-  uncertain_df[,paste0("SD.", spID)] <- uncertain_df$SD
-
-  # add species dataframe to stacked dataframe
-  species_stack <- species_stack %>% full_join(uncertain_df %>% dplyr::select(x,y,paste0("SD.", spID)), by=c("x","y"))
-  
-  print(paste0("Added binary prediction of ", spID, " to the species stack"))
-  
-  rm(uncertain_df)
-}, silent=T)}
-
-head(species_stack)
-
-#- - - - - - - - - - - - - - - - - - - - - -
-## Calculate mean uncertainty ####
-species_stack$SD_mean <- rowMeans(species_stack %>% dplyr::select(-x, -y), na.rm=T)
-
-# save
-save(species_stack, file=paste0(here::here(), "/results/_Maps/SDM_stack_uncertainty_", Taxon_name, ".RData"))
-
-#- - - - - - - - - - - - - - - - - - - - - -
-## Plotting ####
-# mean uncertainty
-png(file=paste0(here::here(), "/figures/Uncertainty_", Taxon_name, ".png"),width=1000, height=1000)
-ggplot(data=species_stack, aes(x=x, y=y, fill=SD_mean))+
-  geom_tile()+
-  ggtitle("Mean standard deviation between species-spcific algorithms")+
-  scale_fill_viridis_c(option="magma")+
+  geom_tile(data=uncertain_df %>% filter(Mean!=0), aes(x=x, y=y, fill=Mean))+
+  ggtitle("Coefficient of variation averaged across SDMs")+
+  scale_fill_viridis_c(option="E")+
   theme_bw()+
   theme(axis.title = element_blank(), legend.title = element_blank(),
         legend.position = c(0.1,0.4))
 dev.off()
 
-while (!is.null(dev.list()))  dev.off()
+#- - - - - - - - - - - - - - - - - - - - - -
+## Map species uncertainty maps ####
 
+plots <- lapply(3:(ncol(uncertain_df)-2), function(s) {try({
+  print(s-2)
+  ggplot()+
+    geom_map(data = world.inp, map = world.inp, aes(map_id = region), fill = "grey80") +
+    xlim(-23, 60) +
+    ylim(31, 75) +
+    
+    geom_tile(data=uncertain_df[!is.na(uncertain_df[,s]) & uncertain_df[,s]>0,], 
+              aes(x=x, y=y, fill=uncertain_df[!is.na(uncertain_df[,s]),s]))+
+    ggtitle(colnames(uncertain_df)[s])+
+    scale_fill_viridis_c(option="E")+
+    theme_bw()+
+    theme(axis.title = element_blank(), legend.title = element_blank(),
+          legend.position = c(0.1,0.4))
+  })
+})
+
+
+require(gridExtra)
+#pdf(file=paste0(here::here(), "/figures/Uncertainty_allSpecies_", Taxon_name, ".pdf"))
+png(file=paste0(here::here(), "/figures/Uncertainty_allSpecies_", Taxon_name, ".png"),width=3000, height=3000)
+do.call(grid.arrange, plots)
+dev.off()
 
