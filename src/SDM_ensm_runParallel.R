89b4e0cca77dc1b78a2b40122ce5b9ca1d8e7e6a
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-09-28 14:07:44 +0200

    Add ectraction of evaluation scores

--- src/SDM_ensm_runParallel.R
+++ src/SDM_ensm_runParallel.R
@@ -1479,6 +1479,36 @@ print(ggplot()+
 dev.off()
 
 
+#- - - - - - - - - - - - - - - - - - - - -
+## Model performance ####
+#- - - - - - - - - - - - - - - - - - - - -
+
+data_eval <- data.frame("SpeciesID"="species", "Kappa"=0, "TSS"=0, "ROC"=0)
+
+# for loop through all species
+for(spID in speciesSub){ try({
+  
+  ## Load probability maps 
+  load(file=paste0(here::here(), "/results/", Taxon_name, "/_SDMs/SDM_biomod_", spID, ".RData")) #biomod_list
+  temp_validation <- biomod_list$validation[,str_detect( colnames(biomod_list$validation), "Testing.data")]
+  temp_validation <- temp_validation[,str_detect( colnames(temp_validation), "EMcaByTSS")]
+  
+  data_eval <- rbind(data_eval, c(spID, temp_validation))
+  
+  print(paste0(spID, " successfully loaded."))
+}, silent=T)}
+
+data_eval <- data_eval %>% filter(SpeciesID!="species") 
+data_eval$Kappa <- as.numeric(data_eval$Kappa)
+data_eval$ROC <- as.numeric(data_eval$ROC)
+data_eval$TSS <- as.numeric(data_eval$TSS)
+
+data_eval; str(data_eval)
+
+write.csv(data_eval, paste0(here::here(), "/results/Model_evaluation_", Taxon_name, ".csv"))
+
+data_eval %>% dplyr::select(-SpeciesID) %>% summarize_all(mean)
+data_eval %>% dplyr::select(-SpeciesID) %>% summarize_all(sd)
 
 #- - - - - - - - - - - - - - - - - - - - -
 ## TRASH? ####
