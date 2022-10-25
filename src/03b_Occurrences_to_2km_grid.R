3eef738c823c94e4d8a7a92ceaf1aa390e8ea6f7
JeMaNd-r <rz90seqi@studserv.uni-leipzig.de>
2022-10-25 08:51:48 +0200

    Rasterize occurrences with terra

--- src/03b_Occurrences_to_2km_grid.R
+++ src/03b_Occurrences_to_2km_grid.R
@@ -10,6 +10,7 @@
 gc()
 library(tidyverse)
 library(here)
+library(terra)
 
 library(raster)
 
@@ -20,110 +21,81 @@ library(raster)
 
 #- - - - - - - - - - - - - - - - - - - - -
 Taxon_name <- "Crassiclitellata"
-speciesNames <- read.csv(file=paste0("./results/Species_list_", Taxon_name, ".csv"))
+speciesNames <- read.csv(file=paste0("./data_raw/Species_list_", Taxon_name, ".csv"))
 
-# Note: to run the other grid, you have to comment & un-comment 4 lines
+# Note: to run the other grid (5km, 10km), you have to comment & un-comment 4 lines
 
 ## load grid
-r <- raster::raster(paste0(here::here(),"/data/grid_2k_0p016.tif"))
+r <- terra::rast(paste0(here::here(),"/data_raw/grid_2k_0p016.tif"))
 
 ## Load occurrence data
 occ <- read.csv(file=paste0(here::here(), "/results/Occurrences_", Taxon_name, ".csv"))
 occ$occ <- 1
-occ_stack <- raster()
 occ_points <- data.frame(x=0, y=0, year=1800)[0,]
 
 speciesNames$Records <- NA
+speciesNames$Records_unique <- NA
 
 #- - - - - - - - - - - - - - - - - - - - - - - 
 ## For loop through all species ####
-for(sp in unique(speciesNames$SpeciesID)){
+for(spID in unique(speciesNames$SpeciesID)){
   
   # ignore errors
   try({
   
   # load occurrences for specific species
-  temp_occ <- occ[occ$SpeciesID==sp & !is.na(occ$SpeciesID), c("x", "y", "occ", "SpeciesID", "year")]
+  temp_occ <- occ[occ$SpeciesID==spID & !is.na(occ$SpeciesID), c("x", "y", "occ", "SpeciesID", "year")]
   
   ## save number of records for later ####
-  speciesNames[speciesNames$SpeciesID==sp,]$Records <- nrow(temp_occ)
-    
-  # make occurrences as SpatialPoints object
-  coordinates(temp_occ) <- ~x+y
+  speciesNames[speciesNames$SpeciesID==spID,]$Records <- nrow(temp_occ)
+  speciesNames[speciesNames$SpeciesID==spID,]$Records_unique <- nrow(temp_occ %>% dplyr::select(-year) %>% unique())
+     
+  # make occurrences as SpatVector object
+  temp_occ <- terra::vect(temp_occ, crs="+proj=longlat +datum=WGS84", geom=c("x", "y"))
   
   # make points to raster
-  occ_grid <- raster::rasterize(temp_occ, r, "occ", fun=sum)
-  occ_year <- raster::rasterize(temp_occ, r, "year", fun=max)
-  names(occ_grid) <- sp
+  occ_grid <- terra::rasterize(temp_occ, r, "occ", fun=sum)
+  occ_year <- terra::rasterize(temp_occ, r, "year", fun=max)
+  names(occ_grid) <- spID
   names(occ_year) <- "year"
 
   # crop to raster extent
-  occ_grid <- raster::mask(occ_grid, r)
-  occ_year <- raster::mask(occ_year, r)
-  
-  # # add to raster stack
-  # occ_stack <- raster::stack(occ_stack, occ_grid)
-  
-  #plot(occ_grid)
+  occ_grid <- terra::mask(occ_grid, r)
+  occ_year <- terra::mask(occ_year, r)
   
   # add to point data frame
-  temp_points <- as.data.frame(rasterToPoints(occ_grid))
-  occ_year <- as.data.frame(rasterToPoints(occ_year))
+  temp_points <- as.data.frame(occ_grid, xy=TRUE, row.names = FALSE)
+  occ_year <- as.data.frame(occ_year, xy=TRUE, row.names = FALSE)
 
   temp_points <- dplyr::full_join(temp_points, occ_year)
 
   occ_points <- dplyr::full_join(occ_points, temp_points, by=c("x", "y", "year"))
   
-  print(paste0(sp, " was now tried to add to raster stack."))
-  print(paste0("It has ", nrow(occ[occ$SpeciesID==sp, c("x", "y", "occ", "SpeciesID")]), " records."))
+  print(paste0(spID, " was now tried to add to raster stack."))
+  print(paste0("It has ", nrow(occ[occ$SpeciesID==spID,] %>% dplyr::select(latitude, longitude) %>% unique()), " records."))
   print("######################################################")
   
   }) # end of try loop
 }
 
-# #- - - - - - - - - - - - - - - - - - - - - - - 
-# ## Transform raster stack to occurrence table ####
-# occ_points <- rasterToPoints(occ_stack)
-# occ_points <- as.data.frame(occ_points)
-
-# ## transform in a long data frame
-# occ_points_long <- occ_points %>% pivot_longer(cols=3:ncol(occ_points), names_to = "SpeciesID", values_to = "occ") %>% filter(!is.na(occ))
-# 
-# plot(occ_stack)
-# 
-# ggplot(occ_points_long, aes(x=x, y=y, col=SpeciesID))+
-#   geom_point()
-  
-
 ## Calculate number of grid cells with presence ####
 speciesNames$NumCells_2km <- 0
 
-for(sp in unique(speciesNames$SpeciesID)){ try({
-  temp_records <- nrow(occ_points) - as.numeric(summary(occ_points[,sp])["NA's"])
-  speciesNames[speciesNames$SpeciesID==sp,"NumCells_2km"] <- temp_records
+for(spID in unique(speciesNames$SpeciesID)){ try({
+  temp_records <- nrow(occ_points) - as.numeric(summary(occ_points[,spID])["NA's"])
+  speciesNames[speciesNames$SpeciesID==spID,"NumCells_2km"] <- temp_records
   temp_records <- 0
 }, silent=T)}
-print("Error messages are fine. They relate to species without records.")
-
-#- - - - - - - - - - - - - - - - - - - - - - -
-# Fix species names Aporr_cali and Aporr_rose
-occ_points$Aporr_rose <- rowSums(cbind(occ_points$Aporr_rose, occ_points$Allol_rose), na.rm=T) 
-occ_points$Aporr_cali <- rowSums(cbind(occ_points$Aporr_cali, occ_points$Nicod_cali), na.rm=T)
-occ_points <- occ_points %>% dplyr::select(-Allol_rose, -Nicod_cali)
-occ_points[occ_points==0] <- NA
-
-head(occ_points)
+#print("Error messages are fine. They relate to species without records.")
 
 #- - - - - - - - - - - - - - - - - - - - - - - 
 ## Save ####
-# # save raster stack
-# raster::writeRaster(occ_stack,file=paste0(here::here(), "/results/OccurrenceGrid_", Taxon_name, ".grd"), format="raster")
 
 # save point data frame
 write.csv(occ_points, file=paste0(here::here(), "/results/Occurrence_rasterized_2km_", Taxon_name, ".csv"), row.names=F)
 
 occ_points %>% pivot_longer(cols=(colnames(occ_points %>% dplyr::select(-x, -y, -year)))) %>% filter(!is.na(value))
-# 27,086 (1990: 19,993) records in total
+# 26,351 (1990: 19,993 [OLD]) records in total
 
 # # save individual species as own files
 # raster::writeRaster(occ_stack, filename=names(occ_stack), bylayer=TRUE, format="GTiff")
